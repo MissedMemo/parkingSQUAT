a737c4e8d726775c2e94eb08cee61838ea202faa
AndyL <missedmemo@gmail.com>
2016-10-21 16:40:05 -0700

    fix initial geolocation zoom to XCode simulated user location

--- src/components/map-view.js
+++ src/components/map-view.js
@@ -7,53 +7,50 @@ const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.01;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-
-//enableHighAccuracy: true,
+/* Suggested coords for XCode mobile simulator...
+( via Simulator menu: Debug/Location/Custom Location )
+latitude: 37.871273,
+longitude: -122.268719
+*/
 
 export default class Map extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      region: {
-        latitude: 37.787359,
-        longitude: -122.408227,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
+      region: null
     };
   }
 
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.updateRegion( position );
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      this.updateRegion( position );
+    });
+  }
 
   updateRegion( position ) {
     const region = {
-      latitude: position.coords.longitude,
-      longitude: position.coords.latitude,
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
       latitudeDelta: LATITUDE_DELTA,
       longitudeDelta: LONGITUDE_DELTA
     };
     this.setState({ region });
   }
 
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.updateRegion( position );
-      },
-      (error) => alert(error.message),
-      {timeout: 20000, maximumAge: 1000}
-    );
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      this.updateRegion( position );
-    });
-  }
-  
   render() {
     return <MapView 
       style = {styles.map}
-      region={this.state.region}
       showsUserLocation = {true}
+      followUserLocation={true}
+      region={this.state.region}
     />;
   }
 }
